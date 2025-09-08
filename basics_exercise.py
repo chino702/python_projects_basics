0d8392fd02ee7cef7d0b8985b803c8e799d48f18
chino702 <chinox3702@gmail.com>
2025-09-08 14:43:04 -0700

    First commit - basics exercises

---
+++ basics_exercise.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print("Hello,", name, "You are able to trade options.")
+else:
+    print("Hello,", name, "You can't trade options.")
---
+++ first_program.py
@@ -0,0 +1,7 @@
+#variables
+
+name = "Chino"
+
+age = 32
+
+print("My name is", name, "and I am", age, "years old." )
