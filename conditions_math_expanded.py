1d8232b6271d110c41f9db19ceb9892edfa9008a
chino702 <chinox3702@gmail.com>
2025-09-08 15:28:32 -0700

    first commit to conditions math expanded

---
+++ conditions_math_expanded.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))
+
+num2 = int(input("Enter second number: "))
+
+print("Sum:", num1 + num2 )
+print("Diffrence:", num1 - num2 )
+print("Product:", num1 * num2 )
+print("Quotient:", num1 / num2 )
